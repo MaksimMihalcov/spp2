a35aed16c74e187e305e89865f92102ef23b1c3c
MaksimMihalcov <maksiswet@gmail.com>
2022-09-26 16:07:49 +0300

    init commit

---
+++ Faker/Faker.Concole.Debug/Program.cs
@@ -0,0 +1,18 @@
+﻿using Faker;
+
+var faker = new СFaker();
+
+var obj = faker.Create<A>();
+Console.ReadLine();
+
+public class A
+{
+    public int a;
+    public double b;
+    public B w;
+}
+
+public class B
+{
+    public int q;
+}
---
+++ Faker/Faker/Faker.cs
@@ -0,0 +1,36 @@
+﻿namespace Faker
+{
+    public class СFaker
+    {
+        private Random _random;
+        public СFaker()
+        {
+            _random = new Random();
+        }
+        public T Create<T>()
+        {
+            //если ссылочный тип - получить поля класса, пройтись по ним циклом, попроверять на тип и создать значения
+            //если значимый - сгенерировать значение и вернуть
+            var type = typeof(T);
+            return (T)Create(type);
+        }
+
+        private object CreateValueType(Type type)
+        {
+
+        }
+
+        private object Create(Type type)
+        {
+            return GetDefaultValue(type);
+        }
+
+        private static object GetDefaultValue(Type type)
+        {
+            if (type.IsValueType)
+                return Activator.CreateInstance(type);
+            else
+                return null;
+        }
+    }
+}
